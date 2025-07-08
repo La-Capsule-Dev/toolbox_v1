8a2921070c4d9b414a3b6bb784daf00826e0f1c5
just3mpty <aurelien.corre@outlook.fr>
2025-07-08 16:17:38 +0200

    UI: updated buttons display (sheet) | Frontend now cals rust function to print sheet

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,29 +1,33 @@
 import "./App.css";
 import FicheReco from "./components/fiche/FicheReco";
 import TestButton from "./components/testButtons/TestButton";
-import testScripts from './testScripts.json'
+import testScripts from "./testScripts.json";
 
 function App() {
-
-  return (
-    <main className="container">
-      <section className="ficheContainer">
-        <FicheReco />
-      </section>
-      <section className="buttons">
-        {testScripts.map((cat, idx) => (
-          <>
-            <h2>{cat.category}</h2>
-          <div key={idx} className="category">
-            {cat.scripts.map((script, idx) => (
-              <TestButton key={idx} color={cat.color} title={script.name} script={script.scriptPath} />
-            ))}
-          </div>
-          </>
-        ))}
-      </section>
-    </main>
-  );
+    return (
+        <main className="container">
+            <section className="ficheContainer">
+                <FicheReco />
+            </section>
+            <section className="buttons">
+                {testScripts.map((cat, idx) => (
+                    <div key={cat.category}>
+                        <h2>{cat.category}</h2>
+                        <div key={idx} className="category">
+                            {cat.scripts.map((script, idx) => (
+                                <TestButton
+                                    key={idx}
+                                    color={cat.color}
+                                    title={script.name}
+                                    script={script.scriptPath}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </main>
+    );
 }
 
 export default App;
--- frontend/components/fiche/FicheReco.tsx
+++ frontend/components/fiche/FicheReco.tsx
@@ -1,36 +1,32 @@
 import { useState } from "react";
-import { Command } from "@tauri-apps/plugin-shell";
+import { invoke } from "@tauri-apps/api/core";
 
 const FicheReco = () => {
-  const [result, setResult] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const path = "/home/mtpy/Bureau/toolbox/src-tauri/target/release/toolbox"
+    const [result, setResult] = useState<string>("");
+    const [error, setError] = useState<string>("");
+    const path = "./src/core/gathering";
 
-  const printFiche = async () => {
+    const printFiche = async () => {
+        try {
+            const output = await invoke<string>("call_print_checklist", {
+                scriptDir: path,
+            });
+            setResult(output);
+            setError("");
+        } catch (e: any) {
+            setError(e.toString());
+        }
+    };
 
-    try {
-      console.log("Lance la commande");
-
-      const command = Command.create(path);
-
-      const output = await command.execute();
-      console.log("Stock le résultat");
-      setResult(output.stdout);
-      setError("");
-      console.log("stdout:", output.stdout);
-      console.log("stderr:", output.stderr);
-    } catch (e: any) {
-      setError(e.message || "Erreur lors de l'exécution du script");
-    }
-  };
-
-  return (
-    <div className="fiche">
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <p>{result}</p>
-      <button onClick={printFiche}>PRINT</button>
-    </div>
-  );
+    return (
+        <>
+            <button onClick={printFiche}>PRINT</button>
+            <div className="fiche">
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                <p>{result}</p>
+            </div>
+        </>
+    );
 };
 
 export default FicheReco;
