6d28bcc51ed52c2a411eb65746b844b584674ebd
binary-grunt <codegrunt@proton.me>
2025-07-08 09:28:55 +0200

    feat: Add test verify pkgs

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
+PKGS_TO_INSTALL=("dmidecode" "dialog" "sed" "tr" "smartmontools" "skdump" "inxi" "acpi" "xrandr" "python3" "iconv" "enscript" "ps2pdf" "htop" "upower" "hardinfo" "arecord" "mplayer" "cheese" "arecord" "ffmpeg" "mplayer" "alsautils" "curl" "glxgears" "glmark2" "screentest" "libatasmart-bin" "smartctl" "nmon" "iptraf-ng" "s-tui" "stress-ng" "stress")
+
+
 echo "Voulez-vous installer les dépendances nécessaires ? (Oui/Non)"
 read reponse
 
+if [[ "$reponse" =~ ^([oO][uU][iI]|[oO])$ ]]; then
+    sudo apt update
 
-#HACK: Refactoring la loop maybe
-if [[ "$reponse" =~ ^([oO][uU][iI]|[oO])$ ]]
-then
-    sudo apt update &&
-    PKGS_TO_INSTALL=("dmidecode" "dialog" "sed" "tr" "smartmontools" "skdump" "inxi" "acpi" "xrandr" "python3" "iconv" "enscript" "ps2pdf" "htop" "upower" "hardinfo" "arecord" "mplayer" "cheese" "arecord" "ffmpeg" "mplayer" "alsautils" "curl" "glxgears" "glmark2" "screentest" "libatasmart-bin" "smartctl" "nmon" "iptraf-ng" "s-tui" "stress-ng" "stress")
+    echo "Vérification de la disponibilité des paquets :"
+    for pkg in "${PKGS_TO_INSTALL[@]}"; do
+        if ! apt-cache show "$pkg" >/dev/null 2>&1; then
+            echo "$pkg" >> missing_pkgs.txt
+        fi
+    done
 
-    for INSTALL_PKG in "${PKGS_TO_INSTALL[@]}"; do
-        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $INSTALL_PKG)
-        echo "Recherche de $INSTALL_PKG: $PKG_OK"
-        if ! dpkg-query -W --showformat='${Status}\n' $INSTALL_PKG | grep -q "Le paquet est bien installé"; then
-            echo "Pas de $INSTALL_PKG sur le système. Configuration de $INSTALL_PKG."
-            sudo apt-get --yes install $INSTALL_PKG
+    # 2. Installer uniquement si absent
+    for pkg in "${PKGS_TO_INSTALL[@]}"; do
+        if dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "install ok installed"; then
+            echo "✔ $pkg est déjà installé."
+        else
+            echo "🔧 Installation de $pkg ..."
+            sudo apt-get --yes install "$pkg"
         fi
     done
 else
     echo "L'installation des dépendances a été annulée."
 fi
-
