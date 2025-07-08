b1474b592a255fc72fe7b9badc124d1ce85d889f
binary-grunt <codegrunt@proton.me>
2025-07-08 14:41:36 +0200

    feat: add archived folder

---
+++ src-tauri/archived/TUI.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export LANG=C.UTF-8
+
+install_tui(){
+
+    REQUIRED_GNOME=("gnome-terminal")
+    printf "\e[8;22;50t"
+    source "./../utils/loop-pkgs.sh"
+
+    install_pkgs REQUIRED_GNOME "gnome-terminal"
+
+}
+
+install_tui
