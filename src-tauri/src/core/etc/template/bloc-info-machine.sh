686ff02cb965a31029430114d79a08ae3c895e3b
binary-grunt <codegrunt@proton.me>
2025-07-11 12:19:59 +0200

    feat: Add var_hardware.sh script for hardware variables

--- src-tauri/src/core/etc/template/bloc-info-machine.sh
+++ src-tauri/src/core/etc/template/bloc-info-machine.sh
@@ -5,47 +5,31 @@ TEMPLATE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 DIR_ROOT="${TEMPLATE_DIR%%/core*}/core"
 source "$DIR_ROOT/lib/hardware/init.sh"
 
-batterie=$(battery_parser)
-batmod=$(batmod_parser)
-power=$(power_parser)
-ecran=$(ecran_parser)
-taille=$(taille_parser)
-telecrante=$(telecrante_parser)
-graphique=$(graphique_parser)
-mem=$(mem_parser)
-reseau=$(reseau_parser)
-arch=$(arch_get)
-cpu_model=$(cpu_parser)
-disque=$(disque_parser)
-marque=$(marque_get)
-model=$(model_get)
-serial=$(serial_get)
-
 afficher_bloc_info_machine() {
     cat <<EOF
----------------------------------------------------------------------------| Machine |
+----------------------------------------------------------------------------| Machine |
 Marque          : $marque
 Modèle          : $model
 NumSérie        : $serial
-------------------------------------------------------------------------| Processeur |
+-------------------------------------------------------------------------| Processeur |
 Architecture    : $arch
 $cpu_model
------------------------------------------------------------------------| Mémoire RAM |
+------------------------------------------------------------------------| Mémoire RAM |
 $mem
------------------------------------------------------------------| Stockage de masse |
+------------------------------------------------------------------| Stockage de masse |
 $disque
----------------------------------------------------------------| État de la batterie |
+----------------------------------------------------------------| État de la batterie |
 $batterie
 $batmod
 $power
---------------------------------------------------------------------| Cartes réseaux |
+---------------------------------------------------------------------| Cartes réseaux |
 $reseau
------------------------------------------------------------------| Cartes graphiques |
+------------------------------------------------------------------| Cartes graphiques |
 $graphique
--------------------------------------------------------------------------| Affichage |
+--------------------------------------------------------------------------| Affichage |
 $telecrante
 $taille
---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 Pour tout commentaire supplémentaire, merci d'écrire proprement au dos de cette feuille
 EOF
 }
