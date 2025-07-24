d0752beb3afb5968e781260d0960392e991b114d
binary-grunt <codegrunt@proton.me>
2025-07-24 18:32:42 +0200

    refactor: Remove duplicate package 'hardinfo' from package lists

--- src-tauri/src/core/etc/config/pkgs-list.sh
+++ src-tauri/src/core/etc/config/pkgs-list.sh
@@ -5,36 +5,36 @@ set -euo pipefail
 
 PKGS_DEBIAN=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-utils
-    hardinfo htop inxi libatasmart-bin nmon ghostscript sed smartmontools
+    htop inxi libatasmart-bin nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_ARCH=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-demos
-    hardinfo htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_FEDORA=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-demos
-    hardinfo2 htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_ALPINE=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-demos
-    hardinfo htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_VOID=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-demos
-    hardinfo htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_GENTOO=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 mesa-progs
-    hardinfo htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
 PKGS_OPENSUSE=(
     acpi alsa-utils cheese curl dmidecode enscript ffmpeg glmark2 Mesa-demo-x
-    hardinfo htop inxi libatasmart nmon ghostscript sed smartmontools
+    htop inxi libatasmart nmon ghostscript sed smartmontools
     stress stress-ng s-tui upower mplayer
 )
